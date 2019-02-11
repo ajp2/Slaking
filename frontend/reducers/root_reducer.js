492235cab125dfb5ac1c0368f5d00acfd5b28b62
Arvind <arvind.puvi@gmail.com>
2019-02-11 22:18:01 +1100

    Setup basic frontend structure with redux store

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = (state = {}, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+// const rootReducer = combineReducers({
+//   entities: entitiesReducer
+// });
+
+export default rootReducer;
