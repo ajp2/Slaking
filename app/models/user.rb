a750b4016e35742d679a251ef13064c8a1c91170
Arvind <arvind.puvi@gmail.com>
2019-02-11 14:10:18 +1100

    Define user table and model

---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniquness: true
+  validates :password_digest, presence: { message: "Password can't be blank "}
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  def find_by_credentials(username, password)
+    @user = User.find_by(username: username)
+    @user && @user.is_password?(password) ? @user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+end
