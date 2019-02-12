ba81d638eaa34f64cc2936baa1aaf925662cff6d
Arvind <arvind.puvi@gmail.com>
2019-02-12 17:00:50 +1100

    Prepare for initial Heroku push

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // import { composeWithDevTools } from "redux-devtools-extension";
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => 
   createStore(
     rootReducer,
     preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger))
+    applyMiddleware(...middlewares)
   );
 
 export default configureStore;
