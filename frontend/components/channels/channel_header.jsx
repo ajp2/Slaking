19508ad642fd6e6e601e22df3dd5dfc322eb7e30
Arvind <arvind.puvi@gmail.com>
2019-02-21 00:46:11 +1100

    Create settings button and style inputs

--- frontend/components/channels/channel_header.jsx
+++ frontend/components/channels/channel_header.jsx
@@ -1,18 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class ChannelHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSettings: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ showSettings: true }, () => document.addEventListener('click', this.closeMenu));
+    console.log(this.state);
+  }
+
+  closeMenu(e) {
+    if (this.settings !== null && !this.settings.contains(e.target)) {
+      this.setState({
+        showSettings: false
+      }, () => document.removeEventListener('click', this.closeMenu));
+    }
+  }
+
   render() {
     return (
       <div className='channel-header'>
-        <h2>{this.props.channel.name}</h2>
+        <h2># {this.props.channel.name}</h2>
         <p>{this.props.channel.description}</p>
 
-        <button>Edit Channel</button>
-        <button>Leave Channel</button>
-        <button>Delete Channel</button>
+        <i className="fas fa-cog settings-dropdown" onClick={this.handleClick} ref={el => (this.settings = el)}></i>
+          {this.state.showSettings ? 
+          <div className="header-settings">
+            <a>Edit Channel</a>
+            <a>Leave Channel</a>
+            <a>Delete Channel</a>
+          </div>
+          : null}
       </div>
     )
   }
 }
 
-export default ChannelHeader
+export default ChannelHeader;
