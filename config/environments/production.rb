4a980305626d853046d2e13a48068fdbefd4c934
AJP <arvind.puvi@gmail.com>
2019-07-29 23:04:23 +1000

    Modify action cable url and request origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,9 +48,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.mount_path = '/cable'
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.url = 'wss://cryptic-escarpment-65990.herokuapp.com/cable'
+  # config.action_cable.url = 'wss://cryptic-escarpment-65990.herokuapp.com/cable'
+  config.action_cable.url = 'wss://www.slaking.tk/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'https://cryptic-escarpment-65990.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = [ 'https://cryptic-escarpment-65990.herokuapp.com', 'http://www.slaking.tk' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
